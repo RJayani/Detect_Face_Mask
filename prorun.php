760059ab24aa881793427698ccf7f3af36b834c2
RJayani <jayani2903@gmail.com>
2025-04-18 22:41:10 +0200

    changes made

---
+++ prorun.php
@@ -0,0 +1,5 @@
+<?php
+shell_exec('C:');
+$output= shell_exec('python detect_mask_video.py');
+print($output);
+?>
---
+++ video.php
@@ -0,0 +1,5 @@
+<?php
+shell_exec('C:');
+$output= shell_exec('C:\Users\R Jayani\OneDrive\Desktop\Mini Project Face\Project\python detect_mask_video.py');
+print($output);
+?>
